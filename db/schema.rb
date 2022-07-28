5da4bfe0c84d68fd6ee10341f46e6c7ec94549be
yoshi23s <classa1990@gmail.com>
2022-07-28 13:19:58 +0900

    basic認証の導入

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2022_07_27_065642) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
-    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
